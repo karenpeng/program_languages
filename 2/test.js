9b8ea2d96410f4b079e80348af4ff99205d8f2ff
Karen <karenpenglabs@gmail.com>
2014-02-25 23:27:58 -0500

    fix bug

--- 2/test.js
+++ 2/test.js
@@ -1,14 +1,13 @@
-//var string = '(+98)';
-var counter = 0;
 var mode;
-var result = [];
-var level = -1;
 var digit = [
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 ];
 
 function cal(string) {
   var totalResult = 0;
+  var result = [];
+  var level = -1;
+  var counter = 0;
   for (var i = 0; i < string.length; i++) {
     if (string[i] === '(') {
       level++;
@@ -25,19 +24,25 @@ function cal(string) {
         result[level] += digit[m];
       }
       if (mode === 'sub' && string[i] == digit[m]) {
-        result[level] -= digit[m];
+        if (counter === 0) {
+          result[level] += digit[m];
+          counter++;
+        } else {
+          result[level] -= digit[m];
+        }
       }
     }
     if (string[i] === ')') {
       level--;
+      counter = 0;
     }
   }
   for (var j = 0; j < result.length; j++) {
     totalResult += result[j];
   }
-  console.log(totalResult);
+  console.log(result, totalResult);
 }
 
 cal('(+98)');
 cal('(+8(-93))');
-cal('(+ 10 (- 50 45) 10 (+ 3 7) 10)');
+cal('(+9(-54)9(+37)9)');
